4a8813e68102eb6b70de1626a8cf180587c7b5e6
Senai <simoseni@gmail.com>
2023-05-17 09:53:36 -0700

    creat util

--- src/Util.java
+++ src/Util.java
@@ -1,3 +1,11 @@
+<<<<<<< HEAD
 public clas   Util{
   // no utility
+=======
+public class Util {
+    public static void main(String[] args) {
+        // Utility
+    }
+
+>>>>>>> c851cf5 (creat util)
 }
